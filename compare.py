c68f0309d26d3ef4352712449d1e0454a63c9445
Bilal Baqar <bbaqar@plumgrid.com>
2017-05-09 16:22:49 +0500

    Adding Regex support
    The script will now compare the yaml structure and then match each value to
    respective REGEX mentioned in the template.
    
    Signed-off-by: Bilal Baqar <bbaqar@plumgrid.com>

--- compare.py
+++ compare.py
@@ -1,5 +1,11 @@
 import yaml
 import sys
+import re
+import sys
+
+
+def match_regex(regex, matchstring):
+    return True if re.search(regex, matchstring) else False
 
 def load_yaml(filename):
     with open(filename, 'r') as stream:
@@ -7,6 +13,7 @@ def load_yaml(filename):
             data =  yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)
+            sys.exit()
     return data
 
 
@@ -15,94 +22,46 @@ def save_yaml(filename,data):
         yaml.dump(data, outfile, default_flow_style=False)
 
 
-def compare_dicts(dct, cpr_dct):
-    for k, v in cpr_dct.iteritems():
-        if k not in dct:
-	     print k + " should be present"
-	     sys.exit()
-        if not isinstance(dct[k], type(cpr_dct[k])):
-	     print k + "  value not same data type"
-	     sys.exit()
-	if isinstance(dct[k], dict):
-	     compare_dicts(dct[k],cpr_dct[k])
-	else:
-	     print 'dct value: '
-	     print dct[k]
-	     print 'cpr_dct value: '
-	     print cpr_dct[k]
-	     print "---------"
-
 def compare_value(val, cpr_val):
-    print "comparing "
-    print val
-    print cpr_val
-    if isinstance(val, type(cpr_val)):
-        if isinstance(val, dict):
-            for k, v in cpr_val.iteritems():
-                if k in val:
-                    if isinstance(val[k], type(cpr_val[k])):
+    #print "comparing "
+    #print val
+    #print cpr_val
+    if isinstance(val, type(cpr_val)): # Checking if data type is the same of both values
+        if isinstance(val, dict): # checking if data type is dictionary
+            for k, v in cpr_val.iteritems(): # for each value in the template dictionary value
+                if k in val: # if the same key exists in deploy.yaml data
+                    if isinstance(val[k], type(cpr_val[k])): # checking if value in both dicts has the same data type
 		        compare_value(val[k],cpr_val[k])
                     else:
         	        print k + "  value not same data type"
         	        sys.exit()
-                elif '<ANYVAL>' in k:
+                elif '<ANYVAL>' in k: # if key is not in deploy.yaml data then check if there is a WILDCARD in template dictionary. 
                     match_cnt = 0
-                    for j, l in val.iteritems():
-                        print "Printing j:"
-                        print j
+                    for j, l in val.iteritems(): # for each key in template dict that has the wildcard, iterate over all the keys in deploy.yaml dict and match values
+                        #print "Printing j:"
+                        #print j
                         compare_value(val[j],cpr_val[k])
-                else: 
+                else: # Key is missing in deploy.yaml data .. Change the lock.
         	    print k + " should be present"
         	    sys.exit()
-
-        #    for k, v in val.iteritems():
-        #        if k in cpr_val:
-        #            if isinstance(val[k], type(cpr_val[k])):
-	#	        compare_value(val[k],cpr_val[k])
-        #            else:
-        #	        print k + "  value not same data type"
-        #	        sys.exit()
-        #        else: 
-	#            anykey = next(iter(cpr_val))
-	#	    print anykey
-	#            if '<ANYVAL>' in anykey:
-	#                compare_value(val[k],cpr_val[anykey])
-	#            else:
-        #	        print k + " should be present"
-        #	        sys.exit()
-
-                #if k not in cpr_val:
-	        #    anykey = next(iter(cpr_val))
-		#    print anykey
-	        #    if '<ANYVAL>' in anykey:
-	        #        compare_value(val[k],cpr_val[anykey])
-	        #    else:
-        	#        print k + " should be present"
-        	#        sys.exit()
-                #if not isinstance(val[k], type(cpr_val[anykey])):
-        	#    print k + "  value not same data type"
-        	#    sys.exit()
-		#compare_value(val[k],cpr_val[k])
-        	#if isinstance(dct[k], dict):
-        	#     compare_value(val[k],cpr_val[k])
-        	#else:
-		#     compare_value(val,cpr_val)
-        elif isinstance(val, list):
-            for i in val:
-	        if isinstance(i, type(cpr_val[0])):
-		    if isinstance(i, list):
+        elif isinstance(val, list): # checking if data type is a list
+            for i in val: # for each item in list (deploy.yaml)
+	        if isinstance(i, type(cpr_val[0])): # matching item with first item of template list
+		    if isinstance(i, list): # if item value is a list then match with first item of first item of template list 
 		        compare_value(i, cpr_val[0][0])
 	            else:
-		        compare_value(i, cpr_val[0])
-		else:
+		        compare_value(i, cpr_val[0]) # if any other data type then just compare with first item of template list
+		else: # data type not same of item value
 		    print i + "  value not same data type"
 		    sys.exit()
 
-        else:
-            print 'val: %s  cpr_val: %s' % (val,cpr_val)
-            print "---------"
-    else: 
-        print k + "  value not same data type"
+        else: # Key exists in the right structure. Checking for corresponding regex.
+            if match_regex(cpr_val,val):
+                print 'PASS: val: %s  cpr_val: %s' % (val,cpr_val)
+            else: 
+                print 'MISMATCH XXXX: val: %s  cpr_val: %s' % (val,cpr_val)
+    else: # data type does not match 
+        #print k + "  value not same data type"
         sys.exit()
 
 dct=load_yaml(sys.argv[1])
